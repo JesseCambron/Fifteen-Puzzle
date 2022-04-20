c9a3fa42c286510176afaf0db6a6c8ad89119ce6
JesseCambron <56899274+JesseCambron@users.noreply.github.com>
2022-04-19 22:43:45 -0400

    Add files via upload

--- fifteen_style.js
+++ fifteen_style.js
@@ -252,7 +252,8 @@ var puzzleBoard = (function () {
               }
           }
           if(correctBoards == 15){
-              window.alert("You win!");    
+              window.alert("You win!");
+              confetti();    
           }
       }
       correctBoards = 0;
@@ -299,3 +300,84 @@ function getImage() {
   
   document.body.style.backgroundImage = "url('https://source.unsplash.com/random?sig="+randomInt+"')";
 }
+function confetti(){
+    let canvas = document.getElementById('confetti');
+    
+    var zindex=1;
+
+	canvas.width = 400;
+	canvas.height = 400;
+	canvas.style.zIndex=zindex;
+    
+    let ctx = canvas.getContext('2d');
+    let pieces = [];
+    let numberOfPieces = 50;
+    let lastUpdateTime = Date.now();
+    
+    function randomColor () {
+        let colors = ['#f00', '#0f0', '#00f', '#0ff', '#f0f', '#ff0'];
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+    
+    function update () {
+        let now = Date.now(),
+            dt = now - lastUpdateTime;
+    
+        for (let i = pieces.length - 1; i >= 0; i--) {
+            let p = pieces[i];
+    
+            if (p.y > canvas.height) {
+                pieces.splice(i, 1);
+                continue;
+            }
+    
+            p.y += p.gravity * dt;
+            p.rotation += p.rotationSpeed * dt;
+        }
+    
+    
+        while (pieces.length < numberOfPieces) {
+            pieces.push(new Piece(Math.random() * canvas.width, -20));
+        }
+    
+        lastUpdateTime = now;
+    
+        setTimeout(update, 1);
+    }
+    
+    function draw () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+        pieces.forEach(function (p) {
+            ctx.save();
+    
+            ctx.fillStyle = p.color;
+    
+            ctx.translate(p.x + p.size / 2, p.y + p.size / 2);
+            ctx.rotate(p.rotation);
+    
+            ctx.fillRect(-p.size / 2, -p.size / 2, p.size, p.size);
+    
+            ctx.restore();
+        });
+    
+        requestAnimationFrame(draw);
+    }
+    
+    function Piece (x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = (Math.random() * 0.5 + 0.75) * 15;
+        this.gravity = (Math.random() * 0.5 + 0.75) * 0.1;
+        this.rotation = (Math.PI * 2) * Math.random();
+        this.rotationSpeed = (Math.PI * 2) * (Math.random() - 0.5) * 0.001;
+        this.color = randomColor();
+    }
+    
+    while (pieces.length < numberOfPieces) {
+        pieces.push(new Piece(Math.random() * canvas.width, Math.random() * canvas.height));
+    }
+    
+    update();
+    draw();
+    }
